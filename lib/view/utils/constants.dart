095e59de8f328abede9665d9255359f41a5b02a9
Haykrissha Cuezon <haykrissha.cuezon@lsu.edu.ph>
2021-09-22 20:44:22 +0800

    first commit

---
+++ lib/view/utils/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFFE56AB3);
+
+const kBackgroundButtonColor = Color(0xFFEF87BE);
+
+const kButtonColor = Color(0xFFF9A3CB);
+const kButdtonColor = Color(0xFFFCBCD7);
+
+const kInputFillColor = Color(0xFFEEEEEE);
+
+const kInputTitleColor = Color(0xFF80194D);
+const kTitleTextStyle = TextStyle(
+  fontFamily: 'Arial Rounded',
+  color: Colors.white,
+);
+const kArialFontStyle = TextStyle(
+  fontFamily: 'Arial Rounded',
+);
+
+const kSelectedTab = kBackgroundColor;
